4bbcca78f822c517e6d08902fb24ec01fbfd4116
Topher Hunt <hunt.topher@gmail.com>
2022-02-04 07:22:28 +0100

    Misc. fixes based on another tutorial:
    https://medium.com/swlh/how-to-create-pdf-in-lambda-using-puppeteer-6355348b8a82

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # WVTest PDF generator service using AWS Lambda & Puppeteer
 
-Thanks to https://shubham-pandey.medium.com/pdf-generation-with-serverless-aws-and-puppeteer-cac3442b1c95
+Created by following this tutorial: https://medium.com/swlh/how-to-create-pdf-in-lambda-using-puppeteer-6355348b8a82
+
+Deployed using https://serverless.com/.
+
+Useful references:
+
+- https://www.serverless.com/framework/docs/
+- https://www.serverless.com/framework/docs/providers/aws/guide/credentials/
+
+
+## Dev setup
+
+- Clone the repository
+- `cd` into the project folder
+- `npm install`
+- If `serverless` isn't already in your PATH, you may need to `alias serverless='node_modules/serverless/bin/serverless.js'`.
+- Give your AWS credentials to Serverless - see steps here: https://www.serverless.com/framework/docs/providers/aws/guide/credentials/
+- `serverless deploy` - deploy changes
+- `serverless info` - view details on deployed service & endpoints
--- index.js
+++ index.js
@@ -2,21 +2,8 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const chromium = require('chrome-aws-lambda');
-const htmlGenerator = require('./reportTemplate');
 
-async function createPdf(url, res) {
-  //options for pdf
-  const options = {
-    format: 'A4',
-    printBackground: true,
-    margin: {
-      bottom: 70,
-      top: 0,
-      left: 0,
-      right: 0,
-    }
-  };
-
+async function createPdf(body_html, header_html, footer_html, res) {
   let browser = null;
 
   try {
@@ -26,25 +13,41 @@ async function createPdf(url, res) {
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
     });
-    // launch a new page
+
     const page = await browser.newPage();
-    // navigate to the specified url
-    await page.goto(event.url);
-    // generate a pdf stream of the page with options
-    const pdfStream = await page.pdf(options);
-    // convert stream to b64
-    const b64 = pdfStream.toString('base64');
+    await page.setContent(body_html);
+    await sleep(200); // Wait for charts to finish rendering
+
+    // Docs: https://github.com/puppeteer/puppeteer/blob/v13.1.3/docs/api.md#pagepdfoptions
+    const pdfStream = await page.pdf({
+      // format: "a4",
+      // printBackground: true,
+      margin: {top: "50px", bottom: "50px", left: "10px", right: "10px"},
+      displayHeaderFooter: true, // TODO: Do we need this?
+      headerTemplate: header_html,
+      footerTemplate: footer_html
+    });
+    const base64 = pdfStream.toString('base64');
     await browser.close();
-    res.send({ statusCode: 200, pdfData: b64 });
+    res.send({statusCode: 200, pdfData: base64});
   } catch (err) {
-    res.send({ statusCode: 500, userMessage: err });
+    res.send({statusCode: 500, userMessage: err});
   }
 }
 
+// POST /generate-pdf with a JSON object having 3 fields:
+// - body_html
+// - header_html
+// - footer_html
 app.post('/generate-pdf', async function (req, res) {
   const body = JSON.parse(req.body.toString());
-  const url = body.get('url');
-  await createPdf(url, res);
+  // const url = body.get('url');
+  await createPdf(
+    body.get("body_html"),
+    body.get("header_html"),
+    body.get("footer_html"),
+    res
+  );
 });
 
 module.exports.handler = serverless(app);
